203f4c95b74d3d09b64659d5f42f8b73bbd279f0
itzin <>
2024-11-28 21:20:18 -0300

    feat: adiciona rotas para se obter estatisticas de uma campanha cherry-pick

--- src/campanhas/campanhas.module.ts
+++ src/campanhas/campanhas.module.ts
@@ -5,9 +5,11 @@ import { Campanhas } from './entities/campanhas.entity'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ArrecadacaoService } from 'src/arrecadacao/arrecadacao.service'
 import { Arrecadacao } from 'src/arrecadacao/entities/arrecadacao.entity'
+import { ProdutosNew } from 'src/produtos/entities/produto.entity'
+import { Categorias } from 'src/categorias/entities/categorias.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Campanhas]), TypeOrmModule.forFeature([Arrecadacao])],
+  imports: [TypeOrmModule.forFeature([Campanhas]), TypeOrmModule.forFeature([Arrecadacao]), TypeOrmModule.forFeature([ProdutosNew]), TypeOrmModule.forFeature([Categorias])],
   controllers: [CampanhasController],
   providers: [CampanhasService, ArrecadacaoService],
 })
